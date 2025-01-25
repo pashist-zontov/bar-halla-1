3c748bc72b6d9a78799f05986ba7174fbb2352bc
Pavel Zorin <sistpavel@gmail.com>
2025-01-26 06:04:39 +1000

    BG found

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
-
+import Image from './components/Image'
+import background from './img/BH_bg.jpg'
 
 
 class AppComp extends React.Component {
@@ -12,6 +13,7 @@ class AppComp extends React.Component {
             <h1>{this.msg}</h1>
             <input placeholder={this.msg}
             onClick={this.inputClick} onMouseEnter={this.mouseOver} />
+            <Image image={background} />
         </div>)
     }
 
